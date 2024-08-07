484f5c4041dc958d381ea21e6b28312bfca6e2f6
chao <chao@fire>
2024-08-07 11:29:19 +0800

    play

--- app.py
+++ app.py
@@ -5,13 +5,6 @@ url = "https://fire.qingyingda.com/v0/scrape"
 payload = {
     "url": "https://suno.com/song/100690f6-3545-4bec-a97c-2f8c18dc6318",
     "pageOptions": {
-        "headers": {},
-        "includeHtml": True,
-        "includeRawHtml": True,
-        "onlyMainContent": True,
-        "replaceAllPathsWithAbsolutePaths": True,
-        "screenshot": True,
-        "fullPageScreenshot": True,
         "waitFor": 50000
     }
 
